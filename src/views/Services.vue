38d528b2cf21387a5a8aff0fca9139f980518b45
drleeroyphd <dennyjchristopher@gmail.com>
2018-07-19 14:30:07 -0700

    Added services page

--- src/views/Services.vue
+++ src/views/Services.vue
@@ -1,5 +1,33 @@
 <template>
-  <div class="services">
-    <h1>This is a page about our services</h1>
+  <div class="services page">
+    <ul>
+      <li>IT consulting</li>
+      <li>network solutions and security</li>
+      <li>virus removal</li>
+      <li>computer diagnostics and repair</li>
+      <li>data recovery</li>
+      <li>HIPAA compliance</li>
+      <li>10+ years experience in IT</li>
+      <li>phishing training available</li>
+      <li>free first consultation!</li>
+    </ul>
   </div>
 </template>
+
+<style>
+  .services {
+    padding: 2px 25px;
+    background-color: rgba(255, 255, 255, 0.7);
+    width: 60%;
+    margin-left: 10%;
+    margin-top: 6%;
+    z-index: 2;
+    display: flex;
+    justify-content: space-around;
+  }
+
+  .services ul li{
+    font-family: glacial, sans-serif;
+    font-size: 2.5em;
+  }
+</style>
